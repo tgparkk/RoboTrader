b6df4223f7798590de8f7c119ae08e3159d48c29
tgparkk <sttgpark@gmail.com>
2026-01-06 21:20:29 +0900

    -db,data

--- config/ml_settings.py
+++ config/ml_settings.py
@@ -11,7 +11,7 @@ class MLSettings:
     """ML 관련 설정"""
 
     # ML 필터 사용 여부
-    USE_ML_FILTER = True  # ML 필터 활성화
+    USE_ML_FILTER = True  # ML 필터 활성화 (리스크 관리 목적)
 
     # ML 모델 파일
     MODEL_PATH = "ml_model.pkl"  # 현재 사용 중인 모델
